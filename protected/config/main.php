4f6b8a12cf6e7ff62acf470e59e10dd04ff689ac
eric lee <kyhleem@gmail.com>
2012-11-15 08:32:17 +1100

    eric update - Image config

--- protected/config/main.php
+++ protected/config/main.php
@@ -105,7 +105,7 @@ return array(
 		// GD or ImageMagick
 		    'driver'=>'GD',
 		// ImageMagick setup path
-		    'params'=>array('directory'=>'d:/ImageMagick'),
+		    'params'=>array('directory'=>'c:/ImageMagick'),
 		),
 	),
 
