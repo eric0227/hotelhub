9770323946de96a96d4473892856cfcd0e899aee
eric lee <kyhleem@gmail.com>
2012-11-23 13:40:39 +1100

    eric - update - view

---
+++ protected/views/construction/index.php
@@ -0,0 +1,4 @@
+<?php
+	$this->pageTitle=Yii::app()->name;
+?>
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/under_construction.jpg" />
--- protected/views/layouts/front/main.php
+++ protected/views/layouts/front/main.php
@@ -1,97 +1,74 @@
-
 <?php
 $service = Yii::app()->session->get('service',1);
 $lang = Yii::app()->session->get('lang', 1);
 $id = Yii::app()->user->id;
+
+$current = "";
+if(Yii::app()->request->pathinfo == '') $current = 'current';
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<meta charset="utf-8" />
 	<meta name="language" content="en" />
-
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/flick/jquery-ui-1.9.1.custom.min.css" />
 	<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/front.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/global.css" />
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<!-- load javascript files -->
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-1.8.2.js" type="text/javascript"></script>
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-ui-1.9.1.custom.min.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/html5shiv.js"></script>
+	<![endif]-->
 </head>
-
 <body>
-
-<div class="container" id="page">
-
-	<div id="header">
-		<div id="logo">
-			<?php /* echo CHtml::encode(Yii::app()->name); */ ?>
-			Holidoy Front
-		</div>
-		<div>
-			<div style="float:left;">
-			<?php 
-				echo CHtml::beginForm( Yii::app()->request->baseUrl .'/service/change','post');
-				echo CHtml::dropDownList('service', $service, Service::items());
-				echo CHtml::submitButton("Service", array('class'=>'btn'));
-				echo CHtml::endForm();
-			?>
+<div id="wrapper">
+	<header id="site_header">
+		<div class="separator">
+			<div id="logo">
+				<a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php">
+					<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/logo.jpg" alt="Holidoy" />
+				</a>
 			</div>
-			
-			<div style="float:left; padding-left: 20px;">
-			<?php 
-				echo CHtml::beginForm( Yii::app()->request->baseUrl .'/lang/change','post');
-				echo CHtml::dropDownList('lang', $lang, Lang::items());
-				echo CHtml::submitButton("Lang", array('class'=>'btn'));
-				echo CHtml::endForm();
-			?>
+			<div id="header_controls">
+				<a href="<?php echo Yii::app()->request->baseUrl; ?>/user" class="btn">Doy Member</a>
+				<select class="language">
+					<option value="Australia">Australia</option>
+					<option value="Korean">Korean</option>			
+				</select>
+				<div class="sns">
+					<h4>Follow Us On</h4>
+					<a href="http://facebook.com/" class="sns_icon facebook" title="Facebook"></a>
+					<a href="http://twitter.com/" class="sns_icon twitter" title="Twitter"></a><br/>
+					<a href="http://plus.google.com/" class="sns_icon googleplus" title="Google Plus"></a>
+					<a href="http://youtube.com/" class="sns_icon youtube" title="Youtube"></a>
+				</div>
+				<a href="<?php echo Yii::app()->request->baseUrl; ?>/sup/login" class="partner_login btn btn-warning">Partner Login</a>
 			</div>
-			<div style="clear:both;"></div>
 		</div>
-	</div><!-- header -->
-	<div id="mainmenu">
-		<?php $this->widget('bootstrap.widgets.TbMenu',array(
-			'type' => 'tabs',
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/index')),
-				array('label'=>'Hotel', 'url'=>array('/frontHotel'), 'tag'=>'product'),
-				array('label'=>'My Page', 'url'=>'',
-					'items'=>array(
-						array('label'=>'User', 'url'=>array('/user/'.$id, 'tag'=>'user')),
-						array('label'=>'Cart', 'url'=>array('/cart/index', 'tag'=>'cart')),
-						array('label'=>'Orders', 'url'=>array('/order/index', 'tag'=>'order')),
-					),
-					'visible'=>!Yii::app()->user->isGuest
-				),
-				array('label'=>'Login', 'url'=>array('/index/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/index/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	
-	<div>
-		<div id="center">
-			<?php echo $content; ?>
-			<div class="clear"></div>
-		</div >
-		<div id="righter">
-			cart..
-		</div>
-		<div class="clear"></div>
-	</div>
-	
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by Holidoy.<br/>
-		All Rights Reserved.<br/>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
+		<nav>
+			<ul id="navigation">
+				<li class="<?php echo $current ?>"><a href="<?php echo Yii::app()->request->baseUrl; ?>" class="menu1"><span>Accommodation</span></a></li>
+				<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/construction" class="menu2"><span>Car Rental<br/>Services</span></a></li>
+				<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/construction" class="menu3"><span>Things To do<br/>Attraction</span></a></li>
+				<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/construction" class="menu4"><span>Day Tour</span></a></li>
+				<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/construction" class="menu5"><span>Hot Deal</span></a></li>
+			</ul>
+		</nav>
+	</header>
+	<?php echo $content; ?>
+</div>
+<footer id="site_footer">
+	Copyright &copy; <?php echo date('Y'); ?> by Holidoy. All Rights Reserved.<br/>
+	Powered by <a href="http://www.gnaemarketing.com.au">Gna eMarketing </a>
+</footer>
 </body>
 </html>
