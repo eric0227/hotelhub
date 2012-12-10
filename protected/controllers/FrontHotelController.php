6067b83f6dbef6864cbfc6a202ef475b3cd51f26
User <User@192.168.1.13>
2012-12-10 13:06:33 +1100

    Chris.

--- protected/controllers/FrontHotelController.php
+++ protected/controllers/FrontHotelController.php
@@ -41,7 +41,7 @@ class FrontHotelController extends Controller
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
 	 */
-	public function actionView($id)
+	public function actionView()
 	{
 		$this->render('view');
 	}
